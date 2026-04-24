5db2343fd2bbaf7ec595881a30221b78058295ae
Alen Lukic <alenlukic13@gmail.com>
2026-04-23 23:50:11 -0400

    re-add rules

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     host: true,
     port: clientPort,
-    allowedHosts: true,
+    allowedHosts: 'all',
     proxy: {
       '/api': `http://127.0.0.1:${apiPort}`,
     },
